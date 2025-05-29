4521f0349aafa2309db318e8adc5b98d58589b21
Magner Lesmes <magles978@gmail.com>
2025-05-29 16:56:51 -0500

    fixxed access property
    - diccCriteriaRequestConfig queda con acceso público

--- src/logic/meta/structure-metadata-handler.ts
+++ src/logic/meta/structure-metadata-handler.ts
@@ -1709,7 +1709,7 @@ export class StructureLogicMetadataHandler<
           keyModule
         );
         diccAC =
-          metadataByModuleContext.__ctrlInstance["diccCriteriaRequestConfig"]; //🐱‍👤Hackeando propiedad protected
+          metadataByModuleContext.__ctrlInstance.diccCriteriaRequestConfig;
       } else {
         throw new LogicError({
           code: ELogicCodeError.MODULE_ERROR,
